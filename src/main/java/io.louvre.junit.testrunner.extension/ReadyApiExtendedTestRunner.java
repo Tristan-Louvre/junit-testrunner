d5b0e19e215ade8fa5ef4ca0c2808a38e8f3b1db
BVJ <byron@wigroup.co.za>
2018-02-08 12:38:17 +0200

    Pass through successful.

--- src/main/java/io.louvre.junit.testrunner.extension/ReadyApiExtendedTestRunner.java
+++ src/main/java/io.louvre.junit.testrunner.extension/ReadyApiExtendedTestRunner.java
@@ -3,5 +3,7 @@ package io.louvre.junit.testrunner.extension;
 import com.smartbear.ready.cmd.runner.pro.SoapUIProTestCaseRunner;
 
 public class ReadyApiExtendedTestRunner extends SoapUIProTestCaseRunner {
-
+    ReadyApiExtendedTestRunner() {
+        super();
+    }
 }
