9c844938b8e3140b5e5f71d169f7545cd7c26db7
BVJ <byron@wigroup.co.za>
2018-02-09 18:11:07 +0200

    Updated project to make use of reduced options.

--- src/main/java/io.louvre.junit.testrunner.extension/ReadyApiExtendedTestCaseRunner.java
+++ src/main/java/io.louvre.junit.testrunner.extension/ReadyApiExtendedTestCaseRunner.java
@@ -1,24 +1,38 @@
 package io.louvre.junit.testrunner.extension;
 
+import com.eviware.soapui.analytics.Analytics;
+import com.eviware.soapui.analytics.ReadyApiActions;
+import com.smartbear.ready.cmd.RunnerProductInfo;
 import com.smartbear.ready.cmd.runner.pro.SoapUIProTestCaseRunner;
 
 public class ReadyApiExtendedTestCaseRunner extends SoapUIProTestCaseRunner {
+    public static void main (String [] var0) throws Exception {
+        RunnerProductInfo.setRunnerName("TestCaseRunner");
+        Analytics.trackAction(ReadyApiActions.START_COMMAND_LINE_RUNNER, new String[]{"Type", "SoapUIProTestCaseRunner"});
+        ReadyApiExtendedTestCaseRunner testCaseRunner = new ReadyApiExtendedTestCaseRunner();
+        int result = testCaseRunner.init(var0);
+        System.exit(result);
+    }
+
     public ReadyApiExtendedTestCaseRunner() {
         super();
     }
 
     @Override
     protected SoapUIOptions initCommandLineOptions() {
-        SoapUIOptions var1 = new SoapUIOptions("testrunner");
-        var1.addOption("c", true, "Sets the testcase");
-        var1.addOption("E", true, "Sets the environment");
-        var1.addOption("f", true, "Sets the output folder to export results to");
-        var1.addOption("I", false, "Do not stop if error occurs, ignore them");
-        var1.addOption("j", false, "Sets the output to include JUnit XML reports");
-        var1.addOption("J", false, "Sets the output to include JUnit XML reports adding test properties to the report");
-        var1.addOption("l", true, "Installs an activated license file");
-        var1.addOption("r", false, "Prints a small summary report");
-        var1.addOption("s", true, "Sets the testsuite");
-        return var1;
+        //SoapUIOptions var1 = super.initCommandLineOptions();
+        //var1.addOption("Z", false, "ITS A FUCKEN Z BRAH");
+        //return var1;
+        SoapUIOptions options = new SoapUIOptions("testrunner");
+        options.addOption("c", true, "Sets the testcase");
+        options.addOption("E", true, "Sets the environment");
+        options.addOption("f", true, "Sets the output folder to export results to");
+        options.addOption("I", false, "Do not stop if error occurs, ignore them");
+        options.addOption("j", false, "Sets the output to include JUnit XML reports");
+        options.addOption("J", false, "Sets the output to include JUnit XML reports adding test properties to the report");
+        options.addOption("l", true, "Installs an activated license file");
+        options.addOption("r", false, "Prints a small summary report");
+        options.addOption("s", true, "Sets the testsuite");
+        return options;
     }
 }
