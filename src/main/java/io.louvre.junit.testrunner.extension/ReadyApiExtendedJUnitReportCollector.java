17ee3abe1bab3d9888fac4c60da17120535af140
BVJ <byron@wigroup.co.za>
2018-02-26 18:44:46 +0200

    Updated gradle file to make the task two in one. "Extended" the JUnitReport Collector with my implementation. Added datasource property key pair values to the generated output. [Bug] the datasource property values are not being set.

--- src/main/java/io.louvre.junit.testrunner.extension/ReadyApiExtendedJUnitReportCollector.java
+++ src/main/java/io.louvre.junit.testrunner.extension/ReadyApiExtendedJUnitReportCollector.java
@@ -1,37 +1,123 @@
 package io.louvre.junit.testrunner.extension;
 
+import com.eviware.soapui.model.TestModelItem;
 import com.eviware.soapui.model.testsuite.*;
 import com.eviware.soapui.report.JUnitReport;
 import com.eviware.soapui.report.JUnitSecurityReportCollector;
+import com.eviware.soapui.support.StringUtils;
 import com.eviware.soapui.support.xml.XmlUtils;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.HashMap;
+import java.util.*;
 
-public class ReadyApiExtendedJUnitReportCollector extends JUnitSecurityReportCollector {
+import org.apache.log4j.Logger;
+
+public class ReadyApiExtendedJUnitReportCollector extends JUnitSecurityReportCollector implements TestRunListener, TestSuiteRunListener, ProjectRunListener {
+    private HashMap<String, JUnitReport> reports;
     private HashMap<TestCase, String> failures;
     private HashMap<TestCase, Integer> errorCount;
+
+    private boolean includeTestPropertiesInReport = false;
     private int maxErrors = 0;
 
-    public ReadyApiExtendedJUnitReportCollector(int maxErrors) {
-        super();
+    private static final Logger log = Logger.getLogger(ReadyApiExtendedJUnitReportCollector.class);
+
+    public ReadyApiExtendedJUnitReportCollector() {
+        this(0);
+    }
+
+    private ReadyApiExtendedJUnitReportCollector(int maxErrors) {
         this.maxErrors = maxErrors;
-        errorCount = new HashMap<TestCase, Integer>();
-        failures = new HashMap<TestCase, String>();
+        reports = new HashMap<>();
+        errorCount = new HashMap<>();
+        failures = new HashMap<>();
+    }
+
+    @Override
+    public void setIncludeTestPropertiesInReport(boolean includeTestPropertiesInReport) {
+        this.includeTestPropertiesInReport = includeTestPropertiesInReport;
+    }
+
+    @Override
+    protected HashMap<String, String> getTestPropertiesAsHashMap(TestModelItem testCase) {
+        HashMap<String, String> testProperties = new HashMap<>();
+        for (Map.Entry<String, TestProperty> stringTestPropertyEntry : testCase.getProperties().entrySet()) {
+            testProperties.put(stringTestPropertyEntry.getKey(), stringTestPropertyEntry.getValue().getValue());
+        }
+        return testProperties;
+    }
+
+    @Override
+    public String getReport() {
+        Set<String> keys = reports.keySet();
+        if (keys.size() > 0) {
+            String key = (String) keys.toArray()[0];
+            return reports.get(key).toString();
+        }
+        return "No reports..:";
+    }
+
+    @Override
+    public HashMap<String, JUnitReport> getReports() {
+        return reports;
+    }
+
+    @Override
+    public void saveReport(JUnitReport report, String filename) throws Exception {
+        report.save(new File(filename));
+    }
+
+    @Override
+    public List<String> saveReports(String path) throws Exception {
+
+        File file = new File(path);
+        if (!file.exists() || !file.isDirectory()) {
+            file.mkdirs();
+        }
+
+        List<String> result = new ArrayList<String>();
+
+        for (String name : reports.keySet()) {
+            JUnitReport report = reports.get(name);
+            String fileName = path + File.separatorChar + "TEST-" + StringUtils.createFileName(name, '_') + ".xml";
+            saveReport(report, fileName);
+            result.add(fileName);
+        }
+
+        return result;
+    }
+
+    @Override
+    public void reset() {
+        reports.clear();
+        failures.clear();
+        errorCount.clear();
+    }
+
+    @Override
+    public void beforeRun(ProjectRunner testScenarioRunner, ProjectRunContext runContext) {
+    }
+
+    @Override
+    public void afterRun(ProjectRunner projectRunner, ProjectRunContext runContext) {
+    }
+
+    @Override
+    public void beforeRun(TestSuiteRunner testRunner, TestSuiteRunContext runContext) {
+    }
+
+    @Override
+    public void afterRun(TestSuiteRunner testRunner, TestSuiteRunContext runContext) {
     }
 
     @Override
     public void beforeRun(TestCaseRunner testRunner, TestCaseRunContext runContext) {
-        HashMap<String, JUnitReport> reports;
-
         TestCase testCase = testRunner.getTestCase();
         TestSuite testSuite = testCase.getTestSuite();
-        reports = this.getReports();
-
-
         if (!reports.containsKey(testSuite.getName())) {
-            JUnitReport report = new ReadyApiExtendedJUnitReport();
+            JUnitReport report = new JUnitReport();
             report.setIncludeTestProperties(this.includeTestPropertiesInReport);
             report.setTestSuiteName(testSuite.getProject().getName() + " - " + testSuite.getName());
             reports.put(testSuite.getName(), report);
@@ -39,9 +125,62 @@ public class ReadyApiExtendedJUnitReportCollector extends JUnitSecurityReportCol
     }
 
     @Override
+    public void afterRun(TestCaseRunner testRunner, TestCaseRunContext runContext) {
+        TestCase testCase = testRunner.getTestCase();
+        JUnitReport report = reports.get(testCase.getTestSuite().getName());
+
+        HashMap<String, String> testProperties = getTestPropertiesAsHashMap(testCase);
+
+        if (TestRunner.Status.INITIALIZED != testRunner.getStatus() && TestRunner.Status.RUNNING != testRunner.getStatus()) {
+            if (TestRunner.Status.CANCELED == testRunner.getStatus()) {
+                report.addTestCase(testCase.getName(), testRunner.getTimeTaken(), testProperties);
+            }
+            if (TestRunner.Status.FAILED == testRunner.getStatus()) {
+                String msg = "";
+                if (failures.containsKey(testCase)) {
+                    msg = failures.get(testCase);
+                }
+                report.addTestCaseWithFailure(testCase.getName(), testRunner.getTimeTaken(), testRunner.getReason(), msg, testProperties);
+            }
+            if (TestRunner.Status.FINISHED == testRunner.getStatus()) {
+                report.addTestCase(testCase.getName(), testRunner.getTimeTaken(), testProperties);
+            }
+        }
+    }
+
+    @Override
+    public void beforeTestSuite(ProjectRunner projectRunner, ProjectRunContext runContext, TestSuite testSuite) {
+        testSuite.addTestSuiteRunListener(this);
+    }
+
+    @Override
+    public void afterTestSuite(ProjectRunner projectRunner, ProjectRunContext runContext, TestSuiteRunner testRunner) {
+        testRunner.getTestSuite().removeTestSuiteRunListener(this);
+    }
+
+    @Override
+    public void beforeTestCase(TestSuiteRunner testRunner, TestSuiteRunContext runContext, TestCase testCase) {
+        testCase.addTestRunListener(this);
+    }
+
+    @Override
+    public void afterTestCase(TestSuiteRunner testRunner, TestSuiteRunContext runContext, TestCaseRunner testCaseRunner) {
+        testCaseRunner.getTestCase().removeTestRunListener(this);
+    }
+
+    @Override
+    public void beforeStep(TestCaseRunner testRunner, TestCaseRunContext runContext, TestStep testStep) {
+    }
+
+    @Override
+    public void beforeStep(TestCaseRunner testRunner, TestCaseRunContext runContext) {
+    }
+
+    @Override
     public void afterStep(TestCaseRunner testRunner, TestCaseRunContext runContext, TestStepResult result) {
         TestStep currentStep = result.getTestStep();
         TestCase testCase = currentStep.getTestCase();
+        TestStep dataSource = testRunner.getTestCase().getTestStepAt(0);
 
         if (result.getStatus() == TestStepResult.TestStepStatus.FAILED) {
             if (maxErrors > 0) {
@@ -57,7 +196,7 @@ public class ReadyApiExtendedJUnitReportCollector extends JUnitSecurityReportCol
                 errorCount.put(testCase, errors + 1);
             }
 
-            StringBuffer buf = new StringBuffer();
+            StringBuilder buf = new StringBuilder();
             if (failures.containsKey(testCase)) {
                 buf.append(failures.get(testCase));
             }
@@ -85,7 +224,21 @@ public class ReadyApiExtendedJUnitReportCollector extends JUnitSecurityReportCol
                 buf.append(XmlUtils.entitize(stringWriter.toString()));
             }
 
-            buf.append("</pre><hr/>More");
+            buf.append("</pre><hr/>");
+
+            String propertyHeader = "----------------- Properties ------------------------------";
+            int indexToInsert = buf.indexOf(propertyHeader) + propertyHeader.length()+1;
+            int propertyCount = dataSource.getPropertyCount();
+
+            log.warn(dataSource.getName());
+
+            for (int i = 0; i < propertyCount; i++) {
+                String propertyKey = dataSource.getPropertyAt(i).getName();
+                String propertyValue = dataSource.getPropertyAt(i).getValue();
+                String propertyConcat = propertyKey + ": " + propertyValue + "\r\n";
+                buf.insert(indexToInsert, propertyConcat);
+                indexToInsert += propertyConcat.length();
+            }
 
             failures.put(testCase, buf.toString());
         }
--- src/main/java/io.louvre.junit.testrunner.extension/ReadyApiExtendedTestCaseRunner.java
+++ src/main/java/io.louvre.junit.testrunner.extension/ReadyApiExtendedTestCaseRunner.java
@@ -9,35 +9,18 @@ import com.smartbear.ready.cmd.runner.pro.SoapUIProTestCaseRunner;
 
 public class ReadyApiExtendedTestCaseRunner extends SoapUIProTestCaseRunner {
 
-    private int maxErrors = 5;
-
     public ReadyApiExtendedTestCaseRunner() {
         super();
     }
 
-    public static void main (String [] var0) throws Exception {
+    public static void main (String [] args) throws Exception {
         RunnerProductInfo.setRunnerName("TestCaseRunner");
         Analytics.trackAction(ReadyApiActions.START_COMMAND_LINE_RUNNER, "Type", "SoapUIProTestCaseRunner");
-        System.exit((new ReadyApiExtendedTestCaseRunner()).init(var0));
-    }
-
-    @Override
-    protected SoapUIOptions initCommandLineOptions() {
-        SoapUIOptions options = new SoapUIOptions("testrunner");
-        options.addOption("c", true, "Sets the testcase");
-        options.addOption("E", true, "Sets the environment");
-        options.addOption("f", true, "Sets the output folder to export results to");
-        options.addOption("I", false, "Do not stop if error occurs, ignore them");
-        options.addOption("j", false, "Sets the output to include JUnit XML reports");
-        options.addOption("J", false, "Sets the output to include JUnit XML reports adding test properties to the report");
-        options.addOption("l", true, "Installs an activated license file");
-        options.addOption("r", false, "Prints a small summary report");
-        options.addOption("s", true, "Sets the testsuite");
-        return options;
+        System.exit((new ReadyApiExtendedTestCaseRunner()).init(args));
     }
 
     @Override
     protected JUnitSecurityReportCollector createJUnitSecurityReportCollector() {
-        return new ReadyApiExtendedJUnitReportCollector(maxErrors);
+        return new ReadyApiExtendedJUnitReportCollector();
     }
 }
